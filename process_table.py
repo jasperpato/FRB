51c1c5c4235fff9696037da3812dad004754afb1
Jasper Paterson <jasperpato@gmail.com>
2023-12-08 16:04:14 +0800

    finished-for-2023

--- process_table.py
+++ process_table.py
@@ -11,6 +11,7 @@ import pandas as pd
 import globalpars
 from utils import *
 import json
+from frb.rm import galactic_rm, load_hutschenreuter2020
 
 
 def split_angle(angle):
@@ -45,7 +46,7 @@ def to_galactic(ra, dec):
 	return sc.l.deg, sc.b.deg
 
 
-def get_error(lon, lat, method='NE2001', width=2, size=30):
+def get_error(lon, lat, method='NE2001', nu=1, width=2, size=30):
 	'''
 	Estimate error for DM and SM by taking size samples within +/- width.
 	'''
@@ -53,7 +54,7 @@ def get_error(lon, lat, method='NE2001', width=2, size=30):
 	lats = np.random.uniform(low=lat - width, high=lat + width, size=size)
 
 	func = lambda lon, lat: get_galactic_dm(lon, lat) if method == 'NE2001' else (
-		np.array([q.value for q in dist_to_dm(lon, lat, globalpars.MW_DIAMETER)])
+		np.array([q.value for q in dist_to_dm(lon, lat, globalpars.MW_DIAMETER, nu=nu)])
 	)
 	ufunc = np.frompyfunc(func, 2, 1)
 
@@ -87,22 +88,23 @@ def complete_row(row, state):
 		row[glon], row[glat] = to_galactic(row[ra], row[dec])
 
 	if not np.isnan([row[glon], row[glat]]).any():
-		# get DM NE2001 and error
+		# DM NE2001 and error
 		row['DM_MW (NE2001)'] = get_galactic_dm(row[glon], row[glat])
 		row['DM_MW error (NE2001)'] = get_error(row[glon], row[glat], method='NE2001')
 
-		#get DM and SM YMW16 and error
-		dm, sc = dist_to_dm(row[glon], row[glat], globalpars.MW_DIAMETER)
-		row['DM_MW (YMW16)'], row['Tau_SC (ms) (YMW16)'] = dm.value, sc.value * 1e3 # ms  
-		row['DM_MW error (YMW16)'], row['Tau_SC error (YMW16)'] = get_error(row[glon], row[glat], method='YMW16')
+		# RM_MW and error
+		coord = SkyCoord(ra=row[ra], dec=row[dec], unit='deg', frame=FK5, equinox='J2000')
+		rm, rm_error = galactic_rm(coord)
+		row['RM_MW (rad/m^2)'], row['RM_MW error'] = rm.value, rm_error.value
 
-	# get DM IGM and error
+	# DM IGM and error
 	if not np.isnan(row['Host z']):
 		row['DM_IGM'] = pcosmic.get_mean_DM(np.array([row['Host z']]), state)[0]
 		row['DM_IGM error'] = get_igm_error(row['Host z'], state)
 
 	# log stellar mass
 	row['Log host mass (M_sun)'] = np.log10(row['Host mass (10^9 M_sun)'] * 10e9)
+	row['Log host mass error'] = row['Host mass error'] / np.log(10) / row['Host mass (10^9 M_sun)'] / 10e9
 
 	return row
 
@@ -111,23 +113,38 @@ def complete_burst_properties(frb, data, output, frb_data):
 	'''
 	Search for FRB in data and fill in burst properties.
 	'''
-	x = data['FRB'] == frb
+	i = data.index[data['FRB'] == frb].to_list()[0]
+	freq = np.mean(frb_data.fmhzarr) * 1e-3 # convert to GHz
 
 	# burst properties
 	b = 'Burst width (ms)'
-	data.loc[x, b] = output[b]
+	data.at[i, b] = output[b]
 
 	s = 'Scattering timescale (ms)'
-	data.loc[x, s], data.loc[x, 'Scattering timescale error'] = output[s]
+	data.at[i, s], data.at[i, 'Scattering timescale error'] = output[s]
 
 	l = 'Linear polarisation fraction'
-	data.loc[x, l], data.loc[x, 'Linear polarisation fraction error'] = output[l]
+	data.at[i, l], data.at[i, 'Linear polarisation fraction error'] = output[l]
 
 	t = 'Total polarisation fraction'
-	data.loc[x, t], data.loc[x, 'Total polarisation fraction error'] = output[t]
+	data.at[i, t], data.at[i, 'Total polarisation fraction error'] = output[t]
 
-	# DM
-	data.loc[x, 'DM_obs'] = frb_data.dm
+	# DM_obs
+	data.at[i, 'DM_obs (pc cm^-3)'] = frb_data.dm
+
+	# YMW16 (requires observed frequency)
+	glon, glat = data.at[i, 'Galactic longitude'], data.at[i, 'Galactic latitude']
+	dm, sc = dist_to_dm(glon, glat, globalpars.MW_DIAMETER, nu=freq)
+	
+	data.at[i, 'DM_MW (YMW16)'], data.at[i, 'Tau_SC (ms) (YMW16)'] = dm.value, sc.value * 1e3 # ms  
+	data.at[i, 'DM_MW error (YMW16)'], data.at[i, 'Tau_SC error (YMW16)'] = get_error(glon, glat, nu=freq, method='YMW16')
+
+	# SC_ex
+	data.at[i, 'SC_ex'] = (data.at[i, 'Scattering timescale (ms)'] - data.at[i, 'Tau_SC (ms) (YMW16)']) ** 0.5
+
+	# DM_ex
+	data.at[i, 'DM_ex (NE2001)'] = data.at[i, 'DM_obs (pc cm^-3)'] - data.at[i, 'DM_MW (NE2001)'] - data.at[i, 'DM_IGM']
+
 
 def update_table(file):
 	'''
@@ -137,7 +154,7 @@ def update_table(file):
 
 	# complete missing host properties
 	data = pd.read_csv(file, index_col=0)
-	# data = data.apply(complete_row, axis=1, state=igm_state)
+	data = data.apply(complete_row, axis=1, state=igm_state)
 	data['FRB'] = data['FRB'].astype(int)
 
 	# iterate through available FRBs and fill in burst properties
@@ -159,5 +176,7 @@ def update_table(file):
 
 
 if __name__ == '__main__':
+	load_hutschenreuter2020()
+
 	file = 'table.csv'
 	update_table(file)
